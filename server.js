c9688b12236922b4ae8d3f57609b7ceb1d68d0ac
Adithya Narayan Rangarajan Sreenivasan <r.s.adithya.narayan@gmail.com>
2020-07-15 14:45:28 +0530

    Second Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
