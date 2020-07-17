e60e6691ab96b87e6b7be0df8b0bb976d75a5cd6
Adithya Narayan Rangarajan Sreenivasan <r.s.adithya.narayan@gmail.com>
2020-07-17 10:28:54 +0530

    profile

--- server.js
+++ server.js
@@ -70,5 +70,43 @@ app.post("/firebaseToken", async function(req, res) {
 
     return res.send(token);
 });
+
+app.post("/profile", async function(req, res) {
+    const token = req.query.token;
+    var resp = "";
+    var request = require("request");
+
+    function callAPI() {
+        return new Promise(function(resolve, reject) {
+            const url = "https://luminus.azure-api.net/user/Profile";
+            request.get(
+                {
+                    url: url,
+                    headers: {
+                        "Ocp-Apim-Subscription-Key":
+                            "c9672e39d6854ec084706e9a944f8b21",
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        Authorization: "Bearer " + token,
+                    },
+                },
+                function(error, response, body) {
+                    console.log(error);
+                    console.log(response);
+                    console.log(body);
+                    if (error) {
+                        resolve(error);
+                    } else {
+                        console.log(body);
+                        resp += body;
+                        resolve(response);
+                    }
+                }
+            );
+        });
+    }
+    await callAPI();
+    return res.send(resp);
+});
+
 // listen on the port
 app.listen(port);
